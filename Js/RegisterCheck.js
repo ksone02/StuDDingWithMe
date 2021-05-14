0bbcd404c35ab65fa3b5a9549a75090fb0569def
ksone02 <eksovus@gmail.com>
2021-05-14 12:35:04 +0900

    revise register-form

--- Js/RegisterCheck.js
+++ Js/RegisterCheck.js
@@ -10,8 +10,11 @@ function checkAll() {
     }
     else if (!check_name(form.name.value)) {
         return false;
+    } 
+    else {
+        alert(form.name.value + "님 회원가입을 축하합니다!");
+        return true;
     }
-    document.form.submit();
 }
 
 function checkExistData(value, dataName) {
@@ -30,8 +33,6 @@ function check_studentNumber(StN) {
     var SNRegExp = /^[0-9]{8,8}$/g;
     if(!SNRegExp.test(StN)) {
         alert("학번은 숫자 8자리로 입력해야합니다!");
-        form.StN.value = "";
-        form.StN.focus();
         return false;
     }
     return true;
@@ -45,8 +46,6 @@ function check_id(id) {
     var idRegExp = /^[a-zA-z0-9]{4,12}$/; //아이디 유효성 검사
     if (!idRegExp.test(id)) {
         alert("아이디는 영문 대소문자와 숫자 4~12자리로 입력해야합니다!");
-        form.userId.value = "";
-        form.userId.focus();
         return false;
     }
     return true;
@@ -64,16 +63,11 @@ function check_id(id) {
     var pwRegExp = /^[a-zA-z0-9]{4,12}$/; //비밀번호 유효성 검사
     if(!pwRegExp.test(pw)) {
         alert("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해야 합니다!");
-        form.pw.value = "";
-        form.pw.focus();
         return false;
     }
 
     if (pw != repw) {
         alert("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
-        form.pw.value = "";
-        form.repw.value = "";
-        form.pw.focus();
         return false;
     }
 
@@ -85,20 +79,6 @@ function check_id(id) {
           return false;
       }
       return true;
-  }
-
-  function check_pw2() {
-    var pw = document.getElementsByClassName("login-pw-input")[0].value;
-    var pw2 = document.getElementsByClassName("login-repw-input")[0].value;
-    var check = document.getElementById("check");
-
-    if(pw != '' && pw2 != '') {
-      if(pw == pw2) {
-        check.innerHTML = "비밀번호가 일치합니다.";
-      } else {
-        check.innerHTML = "비밀번호가 일치하지 않습니다.";
-      }
-    }
   }
 
 
